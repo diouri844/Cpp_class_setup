293d02bd8aececa92f79bc1bd17444faef397dbd
Salah Iddine <SalahIddine.Diouri@usmba.ac.ma>
2022-03-24 13:54:29 +0100

    setup project

---
+++ Module/__init__.py
@@ -0,0 +1,84 @@
+import os
+from tkinter import filedialog
+from tkinter import *
+from PIL import Image, ImageTk
+from tkinter import messagebox
+
+def clear_window_widget(root):
+    root.window.label_titel.destroy()
+    root.window.canva_class_name.destroy()
+    root.window.label_class_name.destroy()
+    root.window.entry_class_name.destroy()
+    root.window.label_class_path.destroy()
+    root.window.canva_class_path.destroy()
+    root.window.entry_class_path.destroy()
+    root.window.btn_select_class_folder.destroy()
+    root.window.btn_next_step.destroy()
+    return
+
+def add_new_attr(root):
+    root.window.titel = Label(root.window, text="Ajouté attribute ", font="Bite-Chocolate 18",
+        bg=root.bg_color, fg=root.fg_color, bd=0)
+
+    
+    # add to window : 
+    root.window.titel.place(relx=0.35, rely=0.1)
+
+
+
+    return
+
+
+
+def next_step(root):
+    if not root.class_name.get() == "" and not root.class_path.get() == "":
+        clear_window_widget(root)
+        # create header file : 
+        try:
+            my_heder_file = open(root.class_path.get()+"/"+root.class_name.get()+".hpp", mode="w")
+            # create body of prototype file : 
+            my_cpp_file = open(root.class_path.get()+"/"+root.class_name.get()+".cpp", mode="w")
+            my_cpp_file.close()
+        except Exception as e:
+            messagebox.showerror("Erreur",e)
+        finally:
+            my_heder_file.close()
+            my_cpp_file.close()
+        # seconde window setup :
+        root.window.geometry("1200x550")
+        # add  a side frame : 
+        root.window.side_frame = Frame(root.window, bg=root.bg_secondary, width=250,height=550)
+        # add liste items : 
+        my_image = Image.open('Images/icons8-ajouter-30.png')
+        my_icon = ImageTk.PhotoImage(my_image)
+        # add a canva btn : 
+        root.window.canva_attr_add = Label(root.window.side_frame, image=my_icon, height=30, width=30, bg=root.bg_secondary)
+        root.window.canva_attr_add.image = my_icon
+        root.window.label_attr_add = Button(root.window.side_frame, text="Ajouter attribute ", font="Bite-Chocolate 12",
+            bg=root.bg_secondary, fg=root.fg_color, bd=0, command=lambda:add_new_attr(root))
+        root.window.canva_mth_add = Label(root.window.side_frame, image=my_icon, height=30, width=30, bg=root.bg_secondary)
+        root.window.canva_mth_add.image = my_icon
+        root.window.label_mth_add = Button(root.window.side_frame, text="Ajouter methode ", font="Bite-Chocolate 12",
+            bg=root.bg_secondary, fg=root.fg_color, bd=0)
+
+        # add to window : 
+        root.window.side_frame.place(relx=0.0, rely=0.0)
+        root.window.canva_attr_add.place(relx=0.0, rely=0.07)
+        root.window.label_attr_add.place(relx=0.15, rely=0.08)
+        root.window.canva_mth_add.place(relx=0.0, rely=0.17)
+        root.window.label_mth_add.place(relx=0.15, rely=0.18)
+    else:
+        messagebox.showerror("Erreur"," Nom class ou chemin class non disponible")
+        root.class_path.set('')
+        root.class_name.set('')
+        root.window.entry_class_name.focus()
+    return
+
+
+def get_output_folder(root):
+    init_path = os.path.expanduser("~")
+    root.window.output_folder = filedialog.askdirectory(initialdir=init_path, title="Choisir un dossier ", 
+    mustexist = True, parent=root.window)
+    root.class_path.set(root.window.output_folder)
+    return 
+
---
+++ main.py
@@ -0,0 +1,66 @@
+from Module import *
+from tkinter import *
+from PIL import Image, ImageTk
+
+
+
+class Class_Editeur():
+    def __init__(self):
+        self.window = Tk()
+        self.bg_color = "white"
+        self.fg_color = "#3479D7"
+        self.bg_secondary = "#ececec"
+        self.class_name = StringVar()
+        self.class_path = StringVar()
+        self.window.geometry("400x550")
+        self.window.config(background=self.bg_color)
+        self.window.resizable(0, 0)
+        self.window.iconbitmap("Images/icons8-c-48.ico")
+        self.window.title(' Editeur des classes Cpp v1')
+        self.window.label_titel = Label(self.window, text="Inisialisé votre class ici ! ",
+            font="Bite-Chocolate 18 underline", bd=0, fg=self.fg_color, bg=self.bg_color)
+        # class setup outils :
+        my_image = Image.open('Images/img_textBox0.png')
+        my_icon = ImageTk.PhotoImage(my_image)
+        self.window.canva_class_name = Label(self.window, image=my_icon, height=40, width=238, bg=self.bg_color)
+        self.window.canva_class_name.image = my_icon
+        self.window.label_class_name = Label(self.window, text="Nom ", bd=0, fg=self.fg_color, bg=self.bg_secondary,
+            font="Bite-Chocolate 11")
+        self.window.entry_class_name = Entry(self.window, highlightthickness=0, 
+            bd=0, bg=self.bg_secondary, textvariable=self.class_name, fg=self.fg_color)
+        
+        self.window.canva_class_path = Label(self.window, image=my_icon, height=40, width=238, bg=self.bg_color)
+        self.window.canva_class_path.image = my_icon
+        self.window.label_class_path = Label(self.window, text="Emplacement ", bd=0, fg=self.fg_color, bg=self.bg_secondary,
+            font="Bite-Chocolate 11")
+        self.window.entry_class_path = Entry(self.window, highlightthickness=0, 
+            bd=0, bg=self.bg_secondary, textvariable=self.class_path, fg=self.fg_color)
+        self.window.btn_select_class_folder = Button(self.window,
+            bg=self.bg_color, fg=self.fg_color, bd=0, text="Ouvrir", font="Bite-Chocolate 8 underline",
+            command=lambda:get_output_folder(self))
+        self.window.btn_next_step = Button(self.window,
+            bg=self.bg_secondary, fg=self.fg_color,bd=0,text="Continuer", font="Bite-Chocolate 10 underline",
+            command=lambda:next_step(self))
+        # entry focus : 
+        self.window.entry_class_name.focus()
+        # add to window :
+        self.window.label_titel.place(relx=0.2, rely=0.01)
+        self.window.label_class_name.place(relx=0.22, rely=0.21)
+        self.window.canva_class_name.place(relx=0.2, rely=0.2)
+        self.window.entry_class_name.place(relx=0.32, rely=0.22, width = 180.0, height=15)
+
+        
+        self.window.label_class_path.place(relx=0.22, rely=0.41)
+        self.window.canva_class_path.place(relx=0.2, rely=0.4)
+        self.window.entry_class_path.place(relx=0.47, rely=0.42, width = 100.0, height=15)
+        self.window.btn_select_class_folder.place(relx=0.67, rely=0.48)
+
+        self.window.btn_next_step.place(relx=0.75, rely=0.75)
+
+    def Run(self):
+        self.window.mainloop()
+        return
+
+if __name__ == "__main__":
+    my_window = Class_Editeur()
+    my_window.Run()
